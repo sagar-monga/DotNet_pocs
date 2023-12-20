14e4d149b6e5ce540614717df3aaf12f3b8b2b9c
Sagar Monga <sagar.monga@grapecity.com>
2023-12-20 12:53:38 +0530

    feat: Integrate env variables in appsettings and launchsettings

--- TestProj2Empty3.1/TestProj2Empty3.1/Startup.cs
+++ TestProj2Empty3.1/TestProj2Empty3.1/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,6 +13,18 @@ namespace TestProj2Empty3._1
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        // Config read in following order
+        // - Appsettings.json,
+        // - appsetting.Env.json
+        // - User Secrets
+        // - Environment Variables
+        // - Command line args
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -32,7 +45,10 @@ namespace TestProj2Empty3._1
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync($"Running on - {System.Diagnostics.Process.GetCurrentProcess().ProcessName}");
+                    await context.Response.WriteAsync($"Running on - {System.Diagnostics.Process.GetCurrentProcess().ProcessName}\n");
+                    //! No output as there is no key found with name "Custom"
+                    // await context.Response.WriteAsync($"Using env variable - {_configuration["Custom"]}");
+                    await context.Response.WriteAsync($"Using env variable - {_configuration["CustomKey"]}");
                 });
             });
         }
