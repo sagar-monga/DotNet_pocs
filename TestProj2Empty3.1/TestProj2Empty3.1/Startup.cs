4fcc811e8c8ed487b2c13a3474a6f6e1d14af6cb
Sagar Monga <sagar.monga@grapecity.com>
2023-12-20 12:26:51 +0530

    build: Fix preLaunchTask error

--- TestProj2Empty3.1/TestProj2Empty3.1/Startup.cs
+++ TestProj2Empty3.1/TestProj2Empty3.1/Startup.cs
@@ -32,7 +32,7 @@ namespace TestProj2Empty3._1
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync($"Running on - {System.Diagnostics.Process.GetCurrentProcess().ProcessName}");
                 });
             });
         }
