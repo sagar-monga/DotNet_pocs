e7bde7e78d5c59621b4373aa48d1352740141782
Sagar Monga <sagar.monga@grapecity.com>
2024-01-17 16:59:46 +0530

    feat: add mvc

---
+++ TestProj2Empty3.1/TestProj2Empty3.1/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TestProj2Empty3._1
+{
+    public class HomeController
+    {
+        public string Index() {
+            return "Hello from MVC";
+        }
+    }
+}
--- TestProj2Empty3.1/TestProj2Empty3.1/Startup.cs
+++ TestProj2Empty3.1/TestProj2Empty3.1/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,77 +31,34 @@ namespace TestProj2Empty3._1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc((options) => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         //* Logger outputs to debug console. SEE OUTPUT TO CHECK HOW REQUEST PROCESSING PIPELINE WORKS
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
-            // Request pipeline using the extension method of the IapplicationBuilder interface
             if (env.IsDevelopment())
             {
-                // middleware pipeline begins here
-                // 1
                 app.UseDeveloperExceptionPage();
             }
 
-            //  Using a seperate file other than the default file as home page
-            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
-            defaultFilesOptions.DefaultFileNames.Clear();
-            defaultFilesOptions.DefaultFileNames.Add("home.html");
-            // app.UseDefaultFiles(defaultFilesOptions);
+            // app.UseFileServer();
+            app.UseStaticFiles();
 
-            //* Order for following 2 middlewares is important
-            // Middleware for using default.html/index.html as home page
-            // app.UseDefaultFiles();
+            // Picks default route with this name : '{controller=Home}/{action=Index}/{id?}' i.e HomeController's INdex function. See definition of middleware
+            // If HomeController's Index is defined, this becomes terminal middleware
+            app.UseMvcWithDefaultRoute();
 
-            // Middleware for static files - include this first
-            // app.UseStaticFiles();
-
-            // Instead of using the 2 middleware above, we can use them together as
-            // Optional, setting path to a different page
-            FileServerOptions fileServerOptions = new FileServerOptions();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("home.html");
-
-            app.UseFileServer();
-
-            // 2
             app.UseRouting();
 
-            // 3a
-            app.Use(async (context, next) =>
-            {
-                logger.LogInformation("MW1: Incoming Request");
-                await context.Response.WriteAsync($"From MW1\n");
-                await next(); // Calls the next middleware, if this is commented, this becomes a TERMINAL middleware, ie not passes to next
-                logger.LogInformation("MW1: Outgoing Response");
-            });
-
-            // 3b
-            app.Use(async (context, next) =>
-            {
-                logger.LogInformation("MW2: Incoming Request");
-                await context.Response.WriteAsync($"From MW2\n");
-                await next(); // Calls the next middleware
-                logger.LogInformation("MW2: Outgoing Response");
-            });
-
-            // 4
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    // Middleware which responds to EVERY Request!
                     await context.Response.WriteAsync($"Running on - {System.Diagnostics.Process.GetCurrentProcess().ProcessName}\n");
-                    //! No output as there is no key found with name "Custom"
-                    // await context.Response.WriteAsync($"Using env variable - {_configuration["Custom"]}");
                     await context.Response.WriteAsync($"Using env variable - {_configuration["CustomKey"]}");
-
-                    logger.LogInformation("MW3: Request handled and response produced");
                 });
-                // To serve static files, they have to be in wwwrroot folder, hence creating one.
-                // But serving them is not handled here
             });
         }
     }
