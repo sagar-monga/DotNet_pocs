d7d8811633525a2bc027c02e0b2721c3988fba58
Sagar Monga <sagar.monga3097@gmail.com>
2024-08-01 17:42:52 +0530

    feat: add a new conrtoller with action methods

---
+++ 01WebApi/01WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01WebApi.Controllers
+{
+
+    [ApiController]
+    // [Route("")] This configures the api to work at the base route. eg is server works at localhost:7033, this get below will be 
+    // present at localhost:7033/
+
+    // [Route("[controller]")]
+    // This sets the base route to https://localhost:7033/Employee
+
+    [Route("myRoute")]
+    public class EmployeeController : ControllerBase
+    {
+        public EmployeeController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Hello");
+        }
+
+        [HttpGet("{id:int}")] // Make sure no spaces
+        public IActionResult Get(int id)
+        {
+            return Ok(new { uuid = id });
+        }
+
+    }
+
+}
