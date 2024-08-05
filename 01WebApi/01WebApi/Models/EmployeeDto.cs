234c0909de57ebdd49983878d86090b0db943eba
Sagar Monga <sagar.monga3097@gmail.com>
2024-08-05 12:50:48 +0530

    fix: correct the formatting

--- 01WebApi/01WebApi/Models/EmployeeDto.cs
+++ 01WebApi/01WebApi/Models/EmployeeDto.cs
@@ -2,10 +2,10 @@
 
 public class EmployeeDto
 {
-    public int Id { get; set; };
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public int? Salary { get; set; };
-    public string? Department { get; set; };
-    public string? Position { get; set; };
+    public int? Salary { get; set; }
+    public string? Department { get; set; }
+    public string? Position { get; set; }
 
 }
