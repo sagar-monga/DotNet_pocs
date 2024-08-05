eb99233a3d4d31d2e9f86151b58ad6089c09b502
Sagar Monga <sagar.monga3097@gmail.com>
2024-08-05 12:48:04 +0530

    feat: add Employee model

---
+++ 01WebApi/01WebApi/Models/EmployeeDto.cs
@@ -0,0 +1,11 @@
+﻿namespace _01WebApi.Models;
+
+public class EmployeeDto
+{
+    public int Id { get; set; };
+    public string Name { get; set; } = string.Empty;
+    public int? Salary { get; set; };
+    public string? Department { get; set; };
+    public string? Position { get; set; };
+
+}
