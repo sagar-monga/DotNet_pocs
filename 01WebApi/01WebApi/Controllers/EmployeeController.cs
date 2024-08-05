b958048efad9beb778faad14b902e37b7d7329d1
Sagar Monga <sagar.monga3097@gmail.com>
2024-08-05 12:22:27 +0530

    feat: add GetAll API action

--- 01WebApi/01WebApi/Controllers/EmployeeController.cs
+++ 01WebApi/01WebApi/Controllers/EmployeeController.cs
@@ -4,15 +4,16 @@ using Microsoft.AspNetCore.Mvc;
 namespace _01WebApi.Controllers
 {
 
-    [ApiController]
+    [ApiController] //* Attribute for improving dev experience like automatic returning 400 on bad request requiring routing etc
     // [Route("")] This configures the api to work at the base route. eg is server works at localhost:7033, this get below will be 
     // present at localhost:7033/
 
     // [Route("[controller]")]
     // This sets the base route to https://localhost:7033/Employee
 
-    [Route("myRoute")]
-    public class EmployeeController : ControllerBase
+    [Route("myRoute")] //* attribute for routing, matches action to controller
+    // public class EmployeeController : Controller //* Controller contains additional functionality like working with Views
+    public class EmployeeController : ControllerBase //* ControllerBase contains basic functionality the Controller needs
     {
         public EmployeeController()
         {
@@ -22,7 +23,11 @@ namespace _01WebApi.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            return Ok("Hello");
+            return new JsonResult(new List<object> {
+                new {id=1, name="Adam"},
+                new {id=2, name="Arthur"},
+                new {id=3, name="Daisy"},
+            });
         }
 
         [HttpGet("{id:int}")] // Make sure no spaces
