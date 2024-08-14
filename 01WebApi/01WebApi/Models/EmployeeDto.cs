02d9549881dbac596cc7b89b15f09b866b50c09c
Sagar Monga <sagar.monga3097@gmail.com>
2024-08-14 13:28:52 +0530

    feat: add calcualted properties

--- 01WebApi/01WebApi/Models/EmployeeDto.cs
+++ 01WebApi/01WebApi/Models/EmployeeDto.cs
@@ -8,16 +8,24 @@ public class EmployeeDto
     public int Salary { get; set; }
     public string Department { get; set; } = string.Empty;
     public string Position { get; set; } = string.Empty;
-    public DateTime? LastWorkingDate {get; set; }
+    public DateTime? LastWorkingDate { get; set; }
     public DateTime HireDate { get; set; }
     public DateTime DateOfBirth { get; set; }
 
-
     // * Calculated Properties
-    // fullName
-    // isActive
-    // AnnualSalary
-    // YearsInCurrentRole
-    // Age
+    // Can do like this too if we want to be more explicit
+    // public string FullName
+    // {
+    //     get
+    //     {
+    //         return FirstName + " " + LastName;
+    //     }
+    // }
 
+    // => creates a read only property
+    public string FullName => FirstName + " " + LastName;
+    public bool IsActive => !LastWorkingDate.HasValue;
+    public long AnnualSalary => (long)Salary * 12;
+    public int YearsInCurrentRole => DateTime.Now.Year - HireDate.Year;
+    public int Age => DateTime.Now.Year - DateOfBirth.Year;
 }
