ead5d8b7d891e1f5d4cdcc60bf019adba962e2dc
Sagar Monga <sagar.monga3097@gmail.com>
2024-10-15 15:37:05 +0530

    feat: add employee entity

---
+++ 01WebApi/01WebApi/Entities/Employee.cs
@@ -0,0 +1,51 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _01WebApi.Entities;
+
+public class Employee
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string LastName { get; set; }
+
+    public int Salary { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Department { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Position { get; set; }
+
+    public DateTime? LastWorkingDate { get; set; }
+
+    [Required]
+    public DateTime HireDate { get; set; }
+
+    [Required]
+    public DateTime DateOfBirth { get; set; }
+
+    public Employee(string firstname, string lastname, int salary, string department, string position, DateTime hireDate, DateTime dateOfBirth)
+    {
+        FirstName = firstname;
+        LastName = lastname;
+        Salary = salary;
+        Department = department;
+        Position = position;
+        HireDate = hireDate;
+        DateOfBirth = dateOfBirth;
+
+    }
+
+}
